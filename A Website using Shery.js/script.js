ca8c2388fd1e3e7debbef524ef0a509e0e5ed781
Adil Ali <adilali49@gmail.com>
2025-03-03 21:12:33 -0800

    add animation to a tags

--- A Website using Shery.js/script.js
+++ A Website using Shery.js/script.js
@@ -2,3 +2,12 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('.main'),
     smooth: true
 });
+
+gsap.from('.home a',{
+    ease: 'power1.out',
+    stagger: .2,
+    delay: 1,
+    duration: 1,
+    y: 10,
+    opacity: 0,
+})
