3f64c1acc49fc193a43647e53d8574185f4bf483
Adil Ali <adilali49@gmail.com>
2025-03-03 21:27:06 -0800

    add animation to ephemeral equilibrium heading

--- A Website using Shery.js/script.js
+++ A Website using Shery.js/script.js
@@ -11,3 +11,12 @@ gsap.from('.home a',{
     y: 10,
     opacity: 0,
 })
+
+Shery.textAnimate(".home > .hero .headings h1", {
+    style: 1,
+    y: 10,
+    delay: 0.1,
+    duration: 1.5,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    multiplier: 0.1,
+  });
